0e50b433b8ea71be4a2df57e0ce21e50a47e0d51
Ella-Y <soph456@naver.com>
2019-10-15 01:28:52 +0900

    웰컴 콜센터

---
+++ 웰컴/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println(".  .   .\n" + 
+				"|  | _ | _. _ ._ _  _\n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.");
+	}
+
+}
