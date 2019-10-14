ffc56006bc3a493b0beafed974524542830e0593
Ella-Y <soph456@naver.com>
2019-10-15 01:29:00 +0900

    저작권

---
+++ 저작권/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		int A=sc.nextInt();
+		int I = sc.nextInt();
+		System.out.println((I-1)*A+1);
+
+	}
+
+}
