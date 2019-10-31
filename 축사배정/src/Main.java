7bddeb74c81da39c9fc5d185cd6c399329b2edb4
Ella-Y <soph456@naver.com>
2019-10-31 20:54:13 +0900

    축사배정 주석 포함

--- 축사배정/src/Main.java
+++ 축사배정/src/Main.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
